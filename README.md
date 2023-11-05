ccaf29cc1c669c28fea3ef1fff84b2c27d0723ce
Momtaz Asmar <122457138+Momtaz1992@users.noreply.github.com>
2023-11-06 00:02:12 +0100

    Project is done

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# weatherApp
+# Weather App Project
+This Weather App is a simple web application that allows users to check the weather conditions for a specific location. It uses HTML, CSS, and JavaScript to create an interactive and user-friendly interface. The app fetches weather data from the OpenWeatherMap API.
+
+Certainly! Here's an example of a short README file for a weather app project using HTML, CSS, JavaScript, and an API:
+
+
+## How to Use
+
+1. Clone the repository to your local machine using `git clone`.
+2. Open `index.html` in your web browser.
+3. Enter the name of the city you want to check the weather for and press the "Search" button.
+4. Wait for a moment, and the app will display the current weather conditions for the specified city.
+
+
--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
 async function getData(city){   
-       const response = await fetch(apiUrl + city + `&appid=${apiKey}`);                                                                                     
-       const data = await response.json() 
-
-       console.log(data)
+       const response = await fetch(apiUrl + city + `&appid=${apiKey}`);   
+       if(response.status ==404){
+              document.querySelector(".error").style.display = "block";
+              document.querySelector(".weather").style.display = "none";
+       }else{
+              const data = await response.json() 
 
        document.querySelector(".city").innerHTML = data.name;
        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
@@ -26,6 +28,10 @@ async function getData(city){
        }else if(data.weather[0].main == "Mist"){
               weatherIcon.src = "./images/mist.png";
        }
+
+       document.querySelector(".weather").style.display = "block";
+       document.querySelector(".error").style.display = "none";
+       }                                                                                  
 }
 searchBtn.addEventListener("click", ()=>{
        getData(searchBox.value);
